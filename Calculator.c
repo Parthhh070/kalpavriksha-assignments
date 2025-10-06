acf3ed128773b82f7a23a3efa470e73cd74da2e5
Parthhh070 <130694139+Parthhh070@users.noreply.github.com>
2025-10-06 12:34:52 +0530

    proper variables names, error handling and code refactoirng is done
    As per the review the changes are done

--- Calculator.c
+++ Calculator.c
@@ -5,69 +5,106 @@
 
 #define MAX 100
 
-int main() {
-    char expr[MAX];
-    printf("Enter expression: ");
-    fgets(expr, MAX, stdin);
 
-    int nums[MAX];      
-    char ops[MAX];      
+void CheckExpression(const char *expression, int numbers[], char operators[], int *numCount, int *opCount);
+int evaluateExpression(int numbers[], char operators[], int numCount, int opCount);
+
+int main() {
+    char expression[MAX];
+    printf("Enter expression: ");
+    fgets(expression, MAX, stdin);
+
+    int numbers[MAX];
+    char operators[MAX];
     int numCount = 0, opCount = 0;
 
+    
+    CheckExpression(expression, numbers, operators, &numCount, &opCount);
+
+    int result = evaluateExpression(numbers, operators, numCount, opCount);
+    printf("Result: %d\n", result);
+
+    return 0;
+}
+
+void CheckExpression(const char *expression, int numbers[], char operators[], int *numCount, int *opCount) {
     int i = 0;
-    while (expr[i]) {
-        if (isspace(expr[i])) {
+    int expectNumber = 1; 
+
+    while (expression[i]) {
+        if (isspace(expression[i])) {
             i++;
             continue;
         }
-        if (isdigit(expr[i])) {
-            int val = 0;
-            while (isdigit(expr[i])) {
-                val = val * 10 + (expr[i] - '0');
+
+        if (isdigit(expression[i])) {
+            int number = 0;
+            while (isdigit(expression[i])) {
+                number = number * 10 + (expression[i] - '0');
                 i++;
             }
-            nums[numCount++] = val;
-        }
-        else if (expr[i] == '+' || expr[i] == '-' || expr[i] == '*' || expr[i] == '/') {
-            ops[opCount++] = expr[i];
+            numbers[(*numCount)++] = number;
+            expectNumber = 0;
+        } 
+        else if (expression[i] == '+' || expression[i] == '-' || 
+                 expression[i] == '*' || expression[i] == '/') {
+            if (expectNumber) {
+                printf("Error: Invalid operator placement near '%c'\n", expression[i]);
+                exit(0);
+            }
+            operators[(*opCount)++] = expression[i];
             i++;
-        }
-        else if (expr[i] == '\n') {
-            break; 
-        }
+            expectNumber = 1;
+        } 
+        else if (expression[i] == '\n') {
+            break;
+        } 
         else {
-            printf("Error: Invalid expression.\n");
-            return 0;
+            printf("Error: Invalid character '%c' in expression.\n", expression[i]);
+            exit(0);
         }
     }
 
-   
-    for (i = 0; i < opCount; i++) {
-        if (ops[i] == '*' || ops[i] == '/') {
-            if (ops[i] == '*') {
-                nums[i] = nums[i] * nums[i+1];
+    if (expectNumber) {
+        printf("Error: Expression cannot end with an operator.\n");
+        exit(0);
+    }
+}
+
+int evaluateExpression(int numbers[], char operators[], int numCount, int opCount) {
+    for (int i = 0; i < opCount; i++) {
+        if (operators[i] == '*' || operators[i] == '/') {
+            if (operators[i] == '*') {
+                numbers[i] *= numbers[i + 1];
             } else {
-                if (nums[i+1] == 0) {
+                if (numbers[i + 1] == 0) {
                     printf("Error: Division by zero.\n");
-                    return 0;
+                    exit(0);
                 }
-                nums[i] = nums[i] / nums[i+1];
+                numbers[i] /= numbers[i + 1];
             }
-            for (int j = i+1; j < numCount-1; j++) nums[j] = nums[j+1];
-            for (int j = i; j < opCount-1; j++) ops[j] = ops[j+1];
+
+            for (int j = i + 1; j < numCount - 1; j++){
+                numbers[j] = numbers[j + 1];
+            }
+            for (int j = i; j < opCount - 1; j++){
+
+                operators[j] = operators[j + 1];
+            }
             numCount--;
             opCount--;
-            i--; 
+            i--;
+        }
+    }
+    int result = numbers[0];
+    for (int i = 0; i < opCount; i++) {
+        if (operators[i] == '+'){
+            result += numbers[i + 1];
+        }
+        else{
+            result -= numbers[i + 1];
         }
     }
 
-    
-    int result = nums[0];
-    for (i = 0; i < opCount; i++) {
-        if (ops[i] == '+') result += nums[i+1];
-        else if (ops[i] == '-') result -= nums[i+1];
-    }
-
-    printf("%d\n", result);
-    return 0;
+    return result;
 }
